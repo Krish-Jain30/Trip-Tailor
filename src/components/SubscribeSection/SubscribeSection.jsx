207510823b99a9339c4b10654edc5549a729243f
Krish30805 <krishjain8700@gmail.com>
2024-10-22 21:49:37 +0530

    updated commit

--- src/components/SubscribeSection/SubscribeSection.jsx
+++ src/components/SubscribeSection/SubscribeSection.jsx
@@ -25,9 +25,6 @@ const SubscribeSection = () => {
           </div>
         </div>
       </div>
-      <div className="absolute max-xl:-bottom-[447%] lg:-bottom-[263%] lg:right-48 xl:-bottom-[227%] xl:right-72 max-xl:right-32 max-md:hidden w-16 h-16 bg-purple-500 rounded-full flex justify-center items-center text-white">
-        <IoIosSend className="text-4xl" />
-      </div>
       <img src={art} alt="" className="absolute w-24 z-10 md:left-7 md:-bottom-[487%] lg:-bottom-[303%] max-md:hidden  xl:left-7 rotate-180 xl:-bottom-[260%]" />
     </div>
   );
